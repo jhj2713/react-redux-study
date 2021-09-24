9a2a5e6b0d5549fb0c17184597495d29f1abff9c
juhyojeong <juhyojeong58@gmail.com>
2021-09-24 09:51:11 +0900

    Simplify write form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import { Route } from 'react-router';
-import NavBar from './components/NavBar';
 import ListPage from './pages/ListPage';
-import WritePage from './pages/WritePage';
-
 
 function App() {
   return (
     <div>
-      <NavBar/>
-      <Route path="/" exact component={ListPage}/>
-      <Route path="/write" exact component={WritePage}/>
+      <ListPage/>
     </div>
   );
 }
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -12,20 +12,48 @@ const StyledItemBox = styled.div`
 `;
 
 function ListPage() {
+    const [Num, setNum] = useState(6)
+    const [Post, setPost] = useState({id: "", title: "", content: ""})
     const [Posts, setPosts] = useState([
-        {id: "1", title: "내용1"},
-        {id: "2", title: "내용2"},
-        {id: "3", title: "내용3"},
-        {id: "4", title: "내용4"},
-        {id: "5", title: "내용5"},
+        {id: "1", title: "제목1", content: "내용1"},
+        {id: "2", title: "제목2", content: "내용2"},
+        {id: "3", title: "제목3", content: "내용3"},
+        {id: "4", title: "제목4", content: "내용4"},
+        {id: "5", title: "제목5", content: "내용5"},
     ])
+
+    const handleWrite = () => {
+        setPosts([...Posts, Post])
+        setNum(Num + 1)
+    }
+    const changeInput = (e) => {
+        setPost({ ...Post, [e.target.name]: e.target.value })
+    }
 
     return (
         <div>
             <h1>글목록</h1>
+            <hr/>
+            <form>
+                <input 
+                    type="text" 
+                    name="title"
+                    value={Post.title} 
+                    placeholder="제목을 입력하세요"
+                    onChange={changeInput}
+                />
+                <input 
+                    type="text" 
+                    name="content"
+                    value={Post.content} 
+                    placeholder="내용을 입력하세요"
+                    onChange={changeInput}
+                />
+                <button type="button" onClick={handleWrite}>글쓰기</button>
+            </form>
             {Posts.map((post) => (
                 <StyledItemBox>
-                    <div>번호 : {post.id}, 제목 : {post.title}</div>
+                    <div>번호 : {post.id}, 제목 : {post.title}, 내용: {post.content}</div>
                     <button>삭제</button>
                 </StyledItemBox>
             ))}
